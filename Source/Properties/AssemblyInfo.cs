dcd90123c0d09e5cd7b44d1eeab58b752295bcbc
José Ángel Soler Ortiz <jose.angel.soler.ortiz@gmail.com>
2023-01-14 10:06:23 +0100

    Clean slate

---
+++ Source/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TerrainMovementKit")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TerrainMovementKit")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("14F91874-7848-4927-A6E4-4B30E15B3307")]
+
+[assembly: AssemblyVersion ("0.1.0")]
